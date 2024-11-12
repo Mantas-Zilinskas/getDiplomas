f76fe28aeadaae606e26af5a1f0ee02b4af8e992
Mantas <mantas.ziln@gmail.com>
2024-11-12 17:23:33 +0200

    can now test if apis work with button

---
+++ System.Server/Controllers/OrderController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace System.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+
+        [HttpPost]
+        public IEnumerable<string> Post([FromBody] IEnumerable<string> values)
+        {
+            Console.WriteLine("I work");
+            return ["value1", "value2"];
+        }
+        //// GET: api/<OrderController>
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/<OrderController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<OrderController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<OrderController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<OrderController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
