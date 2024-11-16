acc766b9c11eb89e5e371145fdeb62fa8d08d040
Mantas <mantas.ziln@gmail.com>
2024-11-16 15:25:58 +0200

    updated product DTO

--- System.Server/Models/DTO/ProductDTO.cs
+++ System.Server/Models/DTO/ProductDTO.cs
@@ -7,5 +7,6 @@
         public string Name { get; set; }
         public Decimal Price { get; set; }
         public int Quantity { get; set; }
+        public List<TaxDTO> Taxes { get; set;}
     }
 }
