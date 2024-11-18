968e486740eee731f0a264541bd0b052e3b0f78a
rytis17 <tritis02@gmail.com>
2024-11-19 00:08:10 +0200

    Added Tax Controller

---
+++ System.Server/Controllers/TaxController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Server.Models.DTO;
+using System.Text.Json;
+
+namespace System.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaxController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<TaxDTO> list = new List<TaxDTO>();
+            list.Add(new TaxDTO());
+            list.Add(new TaxDTO());
+
+            string returnable = JsonSerializer.Serialize(list);
+            return Ok(returnable);
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] TaxDTO tax)
+        {
+            string obj = JsonSerializer.Serialize(tax);
+            Console.WriteLine(obj);
+
+            return Ok(obj);
+        }
+        [HttpGet("{taxId}")]
+        public IActionResult Get(int taxId)
+        {   
+            Console.WriteLine(taxId);
+            var tax = new TaxDTO();
+            string returnable = JsonSerializer.Serialize(tax);
+            
+            return Ok(returnable);
+        }
+        [HttpPut("{taxID}")]
+        public IActionResult Put(int taxId, [FromBody] TaxDTO tax)
+        {
+            string obj = JsonSerializer.Serialize(tax);
+            Console.WriteLine(taxId);
+            Console.WriteLine(obj);
+
+            return Ok(obj);
+        }
+        [HttpDelete("{taxId}")]
+        public IActionResult Delete(int taxId)
+        {
+            Console.WriteLine(taxId);
+            var tax = new TaxDTO();
+            string returnable = JsonSerializer.Serialize(tax);
+
+            return Ok(returnable);
+        }
+
+    }
+}
