ab37a1b573edac9669c8639b4cda014cc94de8bf
Eimantas Šostakas <eimantas.sostakas@mif.stud.vu.lt>
2024-11-27 00:49:30 +0200

    Create IOrderService.cs

---
+++ System.Server/IServices/IOrderService.cs
@@ -0,0 +1,12 @@
+﻿using System.Server.Models;
+
+namespace System.Server.IServices
+{
+    public interface IOrderService
+    {
+        Task AddOrder(Order order);
+        Task UpdateOrder(Order order);  
+        Task RemoveOrder(Order order);
+
+    }
+}
