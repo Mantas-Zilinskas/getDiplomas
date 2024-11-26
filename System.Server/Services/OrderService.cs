fb4895822855a85a2e04a9dfe044f9099f0cb33b
Eimantas Šostakas <eimantas.sostakas@mif.stud.vu.lt>
2024-11-27 01:00:58 +0200

    Create OrderService.cs

---
+++ System.Server/Services/OrderService.cs
@@ -0,0 +1,33 @@
+﻿using System.Server.Data;
+using System.Server.IServices;
+using System.Server.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace System.Server.Services
+{
+    public class OrderService : IOrderService
+    {
+
+        private readonly SystemContext _context;
+        public OrderService(SystemContext context) { _context = context; }
+
+        public async Task AddOrder(Order order)
+        {
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateOrder(Order order)
+        {
+            _context.Orders.Update(order);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveOrder(Order order)
+        {
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
