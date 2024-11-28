92a615230635cb60bd16de5822c30610c145dcfd
Eimantas Šostakas <eimantas.sostakas@mif.stud.vu.lt>
2024-11-28 14:39:31 +0200

    Add Product services class

---
+++ System.Server/IServices/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using System.Server.Models;
+
+namespace System.Server.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<Product>> GetAllProducts();
+        Task<Product> GetProductById(long productId);
+        Task AddProduct(Product product);
+        Task UpdateProduct(Product product);
+        Task DeleteProduct(long productId);
+
+    }
+}
---
+++ System.Server/Services/ProductService.cs
@@ -0,0 +1,50 @@
+﻿using System.Server.Data;
+using System.Server.IServices;
+using System.Server.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace System.Server.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly SystemContext _context;
+
+        public ProductService(SystemContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+
+        public async Task<Product?> GetProductById(long productId)
+        {
+            return await _context.Products.FindAsync(productId);
+        }
+
+        public async Task AddProduct(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateProduct(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteProduct(long productId)
+        {
+            var product = await _context.Products.FindAsync(productId);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
